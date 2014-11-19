89c6d063df9b2b1596a38f6b418a71e2aec17cd1
Frode Aannevik <frode.aa@gmail.com>
2014-11-19 18:32:15 +0100

    remove finished downloades from transmission

--- cron.sh
+++ cron.sh
@@ -20,5 +20,8 @@ echo "$D - running flexget cron now..." >> $LOG
 echo " "
 /usr/local/bin/flexget execute --tasks tv-*
 
+echo "cleanup finished downloads $(transmission-remote --list | grep 100% | grep Done | awk '{print $1}'|wc --lines)"
+transmission-remote --list | grep 100% | grep Done | awk '{print $1}' | xargs --max-args 1 --replace=% transmission-remote --torrent '%' --remove
+
 exit 0
 
