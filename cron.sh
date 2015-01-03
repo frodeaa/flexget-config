b2794735d33009f9925b17fb3077aae3da2a045c
Frode Aannevik <frode.aa@gmail.com>
2015-01-03 22:21:05 +0100

    cleanup transmission and old downloads
    clear complete downloads from transmission
    and remove folders and files that is
    downloaded date is 7 days ago

--- cron.sh
+++ cron.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 FILE="${HOME}/.flexget/.config-lock"
 LOG="${HOME}/.flexget/cron.log"
@@ -20,8 +19,23 @@ echo "$D - running flexget cron now..." >> $LOG
 echo " "
 /usr/local/bin/flexget execute --tasks tv-*
 
-echo "cleanup finished downloads $(transmission-remote --list | grep 100% | grep Done | awk '{print $1}'|wc --lines)"
+D=$(date)
+C=$(transmission-remote --list | grep 100% | grep Done | awk '{print $1}'|wc --lines)
+echo "$D - cleanup finished downloads, found $C" >> $LOG
 transmission-remote --list | grep 100% | grep Done | awk '{print $1}' | xargs --max-args 1 --replace=% transmission-remote --torrent '%' --remove
+
+# PATH where downloads are saved
+DOWNLOADS=$(grep -Eo "path:..(.*)" config.yml |cut -d: -f2 | tr -d ' ')
+
+D=$(date)
+C=$(find $DOWNLOADS -maxdepth 1 -type f -mtime +7 -print|wc --lines)
+echo "$D - remove old download files, found $C" >> $LOG
+find $DOWNLOADS -maxdepth 1 -type f -mtime +7 -print -exec rm {} \;
+
+D=$(date)
+C=$(find $DOWNLOADS -maxdepth 1 -type d -mtime +7 -print|wc --lines)
+echo "$D - remove old downloads directories, found $C" >> $LOG
+find $DOWNLOADS -maxdepth 1 -type d -mtime +7 -print -exec rm -rf {} \;
 
 exit 0
 
