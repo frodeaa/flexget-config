239afd51ac7711df4634de0ca624aa3822102301
Frode Aannevik <frode.aa@gmail.com>
2015-04-18 15:51:49 +0200

    cleanup cron script, simplify logging and constants

--- cron.sh
+++ cron.sh
@@ -1,41 +1,52 @@
 #!/usr/bin/env bash
 
-FILE="${HOME}/.flexget/.config-lock"
-LOG_PATH="${HOME}/.flexget/logs"
-LOG="${LOG_PATH}/cron.log"
+ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+FILE="${ROOT}/.config-lock"
+LOG_PATH="${ROOT}/logs"
 FLEX_LOG="${LOG_PATH}/flexget.log"
 
 mkdir -p $LOG_PATH
 
-F=$(pidof -s -x flexget)
+function log_msg() {
+    echo "$(date) - $@" >> "${LOG_PATH}/cron.log"
+}
+
+F=$(pidof flexget | head -n 1)
 
 if [ $F ]; then
   kill $F
-  echo "$(date) - flexget was still running... so killed it..." >> $LOG
+  log_msg "flexget was still running... so killed it..."
 fi
 
 if [ -f $FILE ]; then
-  echo "$(date) - lock file was there. so deleting lock file..." >> $LOG
+  log_msg "lock file was there. so deleting lock file..."
   rm -f $FILE
 fi
 
-echo "$(date) - running flexget cron now..." >> $LOG
-echo " "
+log_msg "running flexget cron now..."
 
 /usr/local/bin/flexget --logfile $FLEX_LOG execute --tasks tv-* > /dev/null 2>&1
 
-C=$(transmission-remote --list | grep 100% | grep Done | awk '{print $1}'|wc --lines)
-echo "$(date) - cleanup finished downloads, found $C" >> $LOG
-transmission-remote --list | grep 100% | grep Done | awk '{print $1}' | xargs --max-args 1 --replace=% transmission-remote --torrent '%' --remove
+C=$(transmission-remote --list | grep 100% | grep Done | awk '{print $1}'|wc -l)
+log_msg "cleanup finished downloads, found $C"
+
+transmission-remote --list | grep 100% | grep Done | awk '{print $1}'\
+    | xargs --max-args 1 --replace=% transmission-remote --torrent '%' --remove
 
 # PATH where downloads are saved
-DOWNLOADS=$(grep -Eo "path:..(.*)" ${HOME}/.flexget/secrets.yml | cut -d: -f2 | tr -d ' ')
+DOWNLOADS=$(grep -Eo "path:..(.*)" ${ROOT}/secrets.yml | cut -d: -f2 | tr -d ' ')
 
-echo "$(date) Check old downloads in ${DOWNLOADS}" >> $LOG ;
-echo "$(date) - remove old download files, found $(find $DOWNLOADS -maxdepth 1 -type f -mtime +7 -print|wc --lines)" >> $LOG ;
+function count_old() {
+    local ft="$1"
+    find $DOWNLOADS -maxdepth 1 -type $ft -mtime +7 -print|wc -l)
+}
+
+log_msg "Check old downloads in ${DOWNLOADS}"
+
+log_msg "remove old download files, found $(count_old f)"
 find "$DOWNLOADS" -maxdepth 1 -type f -mtime +7 -exec rm {} \;
 
-echo "$(date) - remove old downloads directories, found $(find $DOWNLOADS -maxdepth 1 -type d -mtime +7 -print|wc --lines)" >> $LOG ;
+log_msg"remove old downloads directories, found $(count_old d)"
 find "$DOWNLOADS" -maxdepth 1 -type d -mtime +7 -exec rm -rf {} \;
 
 exit 0
